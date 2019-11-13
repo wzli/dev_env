27dee63504444920d1f2ee65523815e457150b1f
WenZheng Li <me@wenzheng.li>
2019-11-13 23:09:17 +0900

    map port 22 to port 22

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 image=${1:-dev_env}
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker run -dit --rm -P --privileged -v /dev:/dev -v /tmp -v /var -v $script_dir/home:/home $image
+docker run -dit --rm -p 22:22 --privileged -v /dev:/dev -v /tmp -v /var -v $script_dir/home:/home $image
