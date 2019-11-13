ab22ef4bee5d8c8f69fca9edda8ce2700913fed5
WenZheng Li <me@wenzheng.li>
2019-11-13 23:06:53 +0900

    make dev env docker run in background

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 image=${1:-dev_env}
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker run -it --rm -P --privileged -v /dev:/dev -v /tmp -v /var -v $script_dir/home:/home $image
+docker run -dit --rm -P --privileged -v /dev:/dev -v /tmp -v /var -v $script_dir/home:/home $image
