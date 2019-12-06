05d8c1082b7e4ce21924e71a49a658d5b08ca8e1
WenZheng Li <me@wenzheng.li>
2019-12-07 04:19:42 +0900

    added autoformat tools to base

--- run_docker.sh
+++ run_docker.sh
@@ -2,4 +2,4 @@
 image=${1:-"dev_env"}
 port=${2:-2222}
 home_dir=$( cd $(dirname $0) && pwd )/home
-docker run -dit --rm -p $port:22 -p 11311:11311 --privileged -v /lib/modules:/lib/modules -v /dev:/dev -v /tmp -v /var/cache -v $home_dir:/home $image bash -c "service ssh start && sleep infinity"
+docker run -dit --rm -p $port:22 --privileged -v /lib/modules:/lib/modules -v /dev:/dev -v /tmp -v /var/cache -v $home_dir:/home $image bash -c "service ssh start && sleep infinity"
