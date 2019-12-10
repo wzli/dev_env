d4a9e4fe63125ed5aafea511fa30e18472a10242
Sootballs Robot <rr@sb_1.t1>
2019-12-10 22:11:23 +0900

    allow running docker inside docker

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-image=${1:-"dev_env"}
+image=${1:-"wzli/dev_env:base"}
 port=${2:-2222}
 home_dir=$( cd $(dirname $0) && pwd )/home
-docker run -dit --rm -p $port:22 --privileged -v /lib/modules:/lib/modules -v /dev:/dev -v /tmp -v /var/cache -v $home_dir:/home $image bash -c "service ssh start && sleep infinity"
+docker run -dit --rm -p $port:22 --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /dev:/dev -v /tmp -v /var/cache -v $home_dir:/home $image bash -c "service ssh start && sleep infinity"
